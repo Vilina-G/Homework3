4e0e74e02edb81b24a4721679f4ed81a789d1eb3
Vilina <vilina.grafova@mail.ru>
2022-11-24 22:07:24 +0300

    z19

---
+++ z19/Program.cs
@@ -0,0 +1,37 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+// //решение задачи текстом.
+// Console.WriteLine("Введите пятизначное число");
+// string number = Console.ReadLine();
+
+// //Сравниваем первое == пятое число и сравниваем второе == четвертое число
+// if (number[0] == number[4] && number[1] == number[3])
+// {
+//     Console.WriteLine($"{number} -> да");
+// }
+// else
+// {
+//     Console.WriteLine($"{number} -> нет");
+// }
+
+
+//решение математически.
+Console.WriteLine("Введите пятизначное число");
+var number = Convert.ToInt32(Console.ReadLine());
+var first = number / 10000;
+var last = number % 10;
+var second = ((number / 1000) % 10 );
+var four = ((number % 100) / 10);
+
+if (first == last && second == four)
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
+
