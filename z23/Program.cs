918e67ea42ea62e0c87a02ffbd468e1d9c63a2b2
Vilina <vilina.grafova@mail.ru>
2022-11-25 00:37:50 +0300

    z23

---
+++ z23/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+Console.Write("Введите число: ");
+var num = Convert.ToInt32(Console.ReadLine());
+var start_text = "";
+for (int i = 1; i <= num; i++)
+{
+    start_text = start_text.Insert(start_text.Length,Convert.ToString(Math.Pow(i, 3)) + ", "); 
+}
+Console.WriteLine(start_text);
